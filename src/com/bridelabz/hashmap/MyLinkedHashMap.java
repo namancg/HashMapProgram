33a362aef3fed24a6230e2c149bf0d441b373c27
namancg <naman.c@perfios.com>
2021-09-01 20:15:00 +0530

    ADDED: class to create LinkedList with hashcode

---
+++ src/com/bridelabz/hashmap/MyLinkedHashMap.java
@@ -0,0 +1,47 @@
+package com.bridelabz.hashmap;
+
+import java.util.ArrayList;
+
+public class MyLinkedHashMap <K,V>{
+	private final int numberOfBuckets;
+	ArrayList<LinkedList<K>> myBuketArray;
+	public MyLinkedHashMap() {
+		this.numberOfBuckets=10;
+		this.myBuketArray=new ArrayList<LinkedList<K>>(numberOfBuckets);
+		for(int i=0;i<numberOfBuckets;i++) {
+			this.myBuketArray.add(null);
+		}
+	}
+	public int getBucketi(K key) {
+		int hashcode = Math.abs(key.hashCode());
+		int i= hashcode%numberOfBuckets;
+		return i;
+	}
+	public V getKey(K key) {
+		int i = this.getBucketi(key);
+		LinkedList<K> LinkedList = this.myBuketArray.get(i);
+		if(LinkedList == null) return null;
+		Node<K, V> node = (Node<K, V>)LinkedList.find(key);
+		return(node ==null)?null:node.getValue();	
+	}
+	public void add(K key,V value) {
+		int i= this.getBucketi(key);
+		LinkedList<K> LinkedList = this.myBuketArray.get(i);
+		if(LinkedList==null) {
+			LinkedList = new LinkedList<>();
+			this.myBuketArray.set(i, LinkedList);
+		}
+		Node<K, V> node = (Node<K, V>) LinkedList.find(key);
+		if(node ==null) {
+			node = new Node<>(key, value);
+			LinkedList.append(node);
+		}
+		else
+			node.setValue(value);
+	}
+	@Override
+	 public String toString() {
+		 return "MyHashMapNodes{"+myBuketArray+'}';
+	 }
+
+}
